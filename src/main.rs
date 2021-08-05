061b57c8ded9484f5f30c08e7a436b0ea7055ec0
Danny <36959138+DannyStoyanov@users.noreply.github.com>
2021-08-05 15:36:48 +0300

    add minor ui changes

--- src/main.rs
+++ src/main.rs
@@ -10,28 +10,36 @@ use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
 
+    // GUI:
     let app = app::App::default().with_scheme(app::Scheme::Gleam);
-    let mut win = Window::default().with_size(800, 600);
-    let mut originalFrame = Frame::default().size_of(&win).with_pos(-200, 0);
-    let mut resultFrame = Frame::default().size_of(&win).with_pos(200, 0);
-    let mut but = button::Button::new(350,100, 100, 40, "Click");
-    let input = input::Input::new(350, 50, 100, 40, "Pixels ot reduce:");
+    let mut win = Window::default().with_size(1000, 600);
+    let mut originalFrame = Frame::default().size_of(&win).with_pos(-250, 100);
+    let mut resultFrame = Frame::default().size_of(&win).with_pos(250, 100);
+    let mut but = button::Button::new(450, 125, 100, 40, "Click");
+    let mut input = input::Input::new(450, 75, 100, 40, "Pixels to reduce:");
 
+    // Setting frames:
     let mut image = SharedImage::load(Path::new("D:\\Documents\\University\\3rd_course\\Rust\\fltk\\workspace\\data\\sample2.png"))?;
-    image.scale(320, 240, true, true);
-
+    image.scale(400, 275, true, true);
     originalFrame.set_image(Some(image.clone()));
-    resultFrame.set_image(Some(image));
+    originalFrame.set_label("Original image");
+    resultFrame.set_image(Some(image.clone()));
+    resultFrame.set_label("Result image");
 
+    // Button visuals:
+    but.clear_visible_focus();
+    but.set_color(Color::White);
+
+    // Window visuals:
+    win.set_color(Color::DarkCyan);
     win.make_resizable(true);
+
     win.end();
     win.show();
 
     let (s, r) = app::channel::<bool>();
     but.emit(s, true);
-
     let mut resultImage: SharedImage;
-
     while app.wait().unwrap() {
         let seamsNumber = input.value();
         match r.recv() {
@@ -39,17 +47,19 @@ fn main() -> Result<(), Box<dyn Error>> {
                 if msg {
                     let mut img = image_data::ImageData::new(String::from("data/sample2.png")).unwrap();
                     resultFrame.set_image(None::<SharedImage>);
-                    win.set_label("Processing!");
+                    win.set_label("Processing, please wait!");
                     match img.seam_carving(seamsNumber.parse::<u32>().unwrap()) {
                         Ok(true) => {
                             resultFrame.set_image(None::<SharedImage>);
                             resultFrame.hide();
                             resultImage = SharedImage::load(Path::new("D:\\Documents\\University\\3rd_course\\Rust\\seam_carving\\data\\seam_carving.png"))?;
-                            resultImage.scale((320 - seamsNumber.parse::<i32>().unwrap()), 240, true, true); 
-                            // resultImage.scale(320, 240, true, true); 
+                            resultImage.scale((400 - seamsNumber.parse::<i32>().unwrap()), 275, false, true); 
                             resultFrame.set_image(Some(resultImage));
                             resultFrame.show();
                             win.set_label("Ready!");
+                            //
+                            but.hide();
+                            input.set_label("Reduced pixels:");
                         },
                         Ok(false) => (),
                         Err(E) => win.set_label("Error!"),
